a5984add0d5f49b992b54eb2f79c9f178021f252
Татьяна <@DESKTOP-ITD1IV1>
2024-10-17 18:31:43 +0500

    Добавьте файлы проекта.

---
+++ lab2/Program.cs
@@ -0,0 +1,94 @@
+﻿using lab2;
+
+using System.Text.RegularExpressions;
+
+
+internal class Program
+{
+    static double GetDoubleInput(string prompt)
+    {
+        while (true)
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine().Trim();
+
+            // Проверяем, что входная строка содержит только цифры и одну запятую
+            if (Regex.IsMatch(input, @"^-?[0-9]+(\,[0-9]+)?$"))
+            {
+                if (double.TryParse(input, out double result))
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Пожалуйста, введите число.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Пожалуйста, введите число с запятой.");
+            }
+        }
+    }
+
+
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите значение 1 числа(x): ");
+        double field1 = GetDoubleInput("");
+
+        Console.Write("Введите значение 2 числа(y): ");
+        double field2 = GetDoubleInput("");
+
+        Console.Write("Введите значение 3 числа(z): ");
+        double field3 = GetDoubleInput("");
+
+        BaseClass baseInstance = new BaseClass(field1, field2, field3);
+        Console.WriteLine("\nБазовый класс:");
+        Console.WriteLine(baseInstance.ToString());
+
+        // Создаем копию базового класса
+        BaseClass copyInstance = new BaseClass(baseInstance);
+        Console.WriteLine("\nКопия базового класса:");
+        Console.WriteLine(copyInstance.ToString());
+
+        // Приведем к целому типу
+        copyInstance.ToInt();
+        Console.WriteLine("\nПосле приведения к целому типу:");
+        Console.WriteLine(copyInstance.ToString());
+
+        // Создаем экземпляр дочернего класса
+        Console.Write("Введите значение нового поля (true/false): ");
+        bool newField = Convert.ToBoolean(Console.ReadLine());
+        DerivedClass derivedInstance = new DerivedClass(field1, field2, field3, newField);
+        Console.WriteLine("\nДочерний класс:");
+        derivedInstance.PrintInfo();
+
+        // Проверяем состояние нового поля
+        Console.WriteLine($"\nТекущее значение нового поля: {derivedInstance.NewField}");
+
+        // Изменяем значения полей и вызываем методы
+        Console.Write("Введите новое значение x: ");
+        double newField1 = GetDoubleInput("");
+
+        Console.Write("Введите новое значение y: ");
+        double newField2 = GetDoubleInput("");
+
+       Console.Write("Введите новое значение NewField (true/false): ");
+        bool newNewField = Convert.ToBoolean(Console.ReadLine());
+        derivedInstance.Field1 = newField1;
+        derivedInstance.Field2 = newField2;
+        derivedInstance.NewField = newNewField;
+
+        Console.WriteLine("\nНовое значение x: " + derivedInstance.Field1);
+        Console.WriteLine("Новое значение y: " + derivedInstance.Field2);
+        Console.WriteLine($"Новое значение NewField: {derivedInstance.NewField}");
+
+        Console.WriteLine("\nПроверяем состояние нового поля:");
+        derivedInstance.CheckNewField();
+        Console.WriteLine("\nИнформация о объекте после изменений:");
+        derivedInstance.PrintInfo();
+    }
+
+
+}
---
+++ lab2/class.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace lab2
+{
+    public class BaseClass
+    {
+        // Три вещественных поля
+        private double _field1;
+
+        public double Field1
+        {
+            get { return _field1; }
+            set { _field1 = value; }
+        }
+
+        private double _field2;
+
+        public double Field2
+        {
+            get { return _field2; }
+            set { _field2 = value; }
+        }
+
+        private double _field3;
+
+        public double Field3
+        {
+            get { return _field3; }
+            set { _field3 = value; }
+        }
+
+        // Конструктор по умолчанию
+        public BaseClass(double field1 = 0, double field2 = 0, double field3 = 0)
+        {
+            Field1 = field1;
+            Field2 = field2;
+            Field3 = field3;
+        }
+
+        // Конструктор копирования
+        public BaseClass(BaseClass other)
+        {
+            Field1 = other.Field1;
+            Field2 = other.Field2;
+            Field3 = other.Field3;
+        }
+
+        // Метод приведения к целому типу
+        public void ToInt()
+        {
+            Field1 = Math.Truncate(Field1);
+            Field2 = Math.Truncate(Field2);
+            Field3 = Math.Truncate(Field3);
+        }
+
+        // Перегруженный метод ToString()
+        public override string ToString()
+        {
+            return $"x: {Field1}, y: {Field2}, z: {Field3}";
+        }
+
+        // Метод для вычисления произведения трех полей
+        public double ProductFields()
+        {
+            return Field1 * Field2 * Field3;
+        }
+
+        // Метод для поиска наибольшего значения из трех полей
+        public double MaxField()
+        {
+            return Math.Max(Math.Max(Field1, Field2), Field3);
+        }
+    }
+
+    // Дочерний класс
+    public class DerivedClass : BaseClass
+    {
+        // Добавляем новое поле типа bool
+        public bool NewField { get; set; }
+
+        // Конструктор по умолчанию для дочернего класса
+        public DerivedClass(double field1 = 0, double field2 = 0, double field3 = 0, bool newField = false)
+            : base(field1, field2, field3)
+        {
+            NewField = newField;
+        }
+
+        // Метод для проверки состояния поля NewField
+        public void CheckNewField()
+        {
+            Console.WriteLine($"Значение нового поля: {NewField}");
+        }
+
+        // Метод для вычисления суммы трех полей
+        public double SumFields()
+        {
+            return Field1 + Field2 + Field3;
+        }
+
+        // Метод для поиска произведения трех полей
+        public double ProductFields()
+        {
+            return Field1 * Field2 * Field3;
+        }
+
+        // Метод для поиска наибольшего значения из трех полей
+        public double MaxField()
+        {
+            return Math.Max(Math.Max(Field1, Field2), Field3);
+        }
+
+        // Метод для вывода информации о классе
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Информация о объекте:");
+            Console.WriteLine(base.ToString());
+            Console.WriteLine($"Новое поле: {NewField}");
+            Console.WriteLine($"Сумма полей: {SumFields():F2}");
+            Console.WriteLine($"Произведение полей: {ProductFields():F2}");
+            Console.WriteLine($"Наибольшее поле: {MaxField():F2}");
+        }
+
+       
+    }
+}
